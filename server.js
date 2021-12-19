9c2e36d6ddce2054f808c4b82aeb7b6fab7a98d5
Austin Engle <engleau@gmail.com>
2021-12-19 17:51:20 -0500

    adding db.json

--- server.js
+++ server.js
@@ -10,7 +10,13 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get("/notes", (req, res) => res.send("Navigate to /send or /routes"));
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "public/notes.html"))
+);
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "public/index.html"))
+);
+app.get("/notes", (req, res) => res.sendFile(path.join(__dirname, "db")));
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
